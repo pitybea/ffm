869580d0ae40ae090ba2814ae51e844280d80bf9
wang <marx@marx>
2017-06-05 09:28:21 +0800

    commit

--- ffm.cpp
+++ ffm.cpp
@@ -50,25 +50,25 @@ float FFM::ffmFunc(const vector<int>& dims,
                    vector<vector<vector<float>>>* sumCache) {
 
   assert(dims.size() == vals.size() && vals.size() == fields.size());
-  float result = parameters[class_indx].w0;
+  auto result = parameters[class_indx].w0;
   for (size_t i = 0; i < dims.size(); i++) {
-    int fea_dim = dims[i];
-    float fea_val = vals[i];
+    auto fea_dim = dims[i];
+    auto fea_val = vals[i];
     result += parameters[class_indx].w[fea_dim] * fea_val;
   }
   for (size_t i = 0; i < dims.size(); i++) {
-    int fea_dim1 = dims[i];
-    float fea_val1 = vals[i];
-    int field1 = fields[i];
+    auto fea_dim1 = dims[i];
+    auto fea_val1 = vals[i];
+    auto field1 = fields[i];
     
     for (size_t j = 0; j < dims.size(); j++) {
       if (i != j) {
-        int fea_dim2 = dims[j];
-        float fea_val2 = vals[j];
-        int field2 = fields[j];
+        auto fea_dim2 = dims[j];
+        auto fea_val2 = vals[j];
+        auto field2 = fields[j];
         for (int k = 0; k < rank_num; k++) {
-          float tm1 = parameters[class_indx].v[fea_dim1][field2][k] * fea_val1;
-          float tm2 = parameters[class_indx].v[fea_dim2][field1][k] * fea_val2;
+          auto tm1 = parameters[class_indx].v[fea_dim1][field2][k] * fea_val1;
+          auto tm2 = parameters[class_indx].v[fea_dim2][field1][k] * fea_val2;
           result += 0.5f * tm1 * tm2;
           if (sumCache != nullptr) {
             (*sumCache)[i][field2][k] += tm2;
@@ -85,9 +85,9 @@ vector<float> FFM::predict(const vector<int>& dims,
                            const vector<float>& vals,
                            const vector<int>& fields) {
   vector<float> exps(class_num, 1.0f);
-  float sum = 1.0f;
+  auto sum = 1.0f;
   for (int i = 0; i < class_num - 1; i++) {
-    float m = ffmFunc(dims, vals, fields, i);
+    auto m = ffmFunc(dims, vals, fields, i);
     exps[i] = safeExp(-1.0f * m);
     sum += exps[i];
   }
@@ -114,7 +114,7 @@ void FFM::paraUpdate(const vector<int>& dims,
 
   assert(dims.size() == vals.size() && vals.size() == fields.size());
   vector<float> exps(class_num, 1.0f);
-  float sum = 1.0f;
+  auto sum = 1.0f;
   vector<vector<vector<vector<float>>>> sum_cache
       (class_num -1,
        vector<vector<vector<float>>>
@@ -123,22 +123,22 @@ void FFM::paraUpdate(const vector<int>& dims,
         (field_num,
          vector<float>(rank_num, 0.0f))));
   for (int i = 0; i < class_num - 1; i++) {
-    float m = ffmFunc(dims, vals, fields, i, &sum_cache[i]);
+    auto m = ffmFunc(dims, vals, fields, i, &sum_cache[i]);
     exps[i] = safeExp(-1.0 * m);
     sum += exps[i];
   }
   for (int i = 0; i < class_num - 1; i++) {
-    float coef = (class_indx == i ? 1.0f : 0.0f);
-    float dl_dw0 = (coef - exps[i] / sum) * weight;
+    auto coef = (class_indx == i ? 1.0f : 0.0f);
+    auto dl_dw0 = (coef - exps[i] / sum) * weight;
     effective[i].w0 =
         optimizer->ftrlProcess(&ftrl_q[i].w0, &ftrl_z[i].w0,
                                &parameters[i].w0, dl_dw0);
 
     for (size_t j = 0; j < dims.size(); j++) {
-      int fea_dim = dims[j];
-      float fea_val = vals[j];
-      int field = fields[j];
-      float dl_dw = dl_dw0 * fea_val;
+      auto fea_dim = dims[j];
+      auto fea_val = vals[j];
+      auto field = fields[j];
+      auto dl_dw = dl_dw0 * fea_val;
       effective[i].w[fea_dim] =
           optimizer->ftrlProcess(&ftrl_q[i].w[fea_dim],
                                  &ftrl_z[i].w[fea_dim],
@@ -146,7 +146,7 @@ void FFM::paraUpdate(const vector<int>& dims,
                                  dl_dw);
       for (int k = 0; k < field_num; k++) {
         for (int m = 0; m < rank_num; m++) {
-          float dl_dv = dl_dw0 * sum_cache[i][j][k][m] * fea_val;
+          auto dl_dv = dl_dw0 * sum_cache[i][j][k][m] * fea_val;
           effective[i].v[fea_dim][k][m] =
               optimizer->ftrlProcess(&ftrl_q[i].v[fea_dim][k][m],
                                     &ftrl_z[i].v[fea_dim][k][m],
